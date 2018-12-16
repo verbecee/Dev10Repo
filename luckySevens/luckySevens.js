71c3943a012beada3288f5efa5778ae7dc6bb605
verbecee <erin.e.verbeck@gmail.com>
2018-12-15 21:57:49 -0600

    reformat, fix extra divs, toString javascript numbers

--- luckySevens/luckySevens.js
+++ luckySevens/luckySevens.js
@@ -13,42 +13,42 @@ var rollsAtMaxAmountWon = 0;
 // maxAmountWon exceeds currentMoney.
 
 if (inputBet <= 0){
-	alert("Error: please enter a value greater than zero.");
+    alert("Error: please enter a value greater than zero.");
 }
 else {
-	while (currentMoney > 0) {
-		rollCounter++;
+    while (currentMoney > 0) {
+        rollCounter++;
 
-		var dieOne = Math.floor(Math.random() * 6 + 1); 
-		var dieTwo = Math.floor(Math.random() * 6 + 1);
-		var diceSum = dieTwo + dieOne;
-		
-		if (diceSum != 7) {
-			console.log("lost");
-			currentMoney--;
-			console.log(currentMoney)
-		}
-		else {
-			console.log("winner");
-			currentMoney = currentMoney + 4;
-			console.log(currentMoney);
-			
-			if (maxAmountWon < currentMoney){
-				maxAmountWon = currentMoney;
-				rollsAtMaxAmountWon = rollCounter;
-			}
+        var dieOne = Math.floor(Math.random() * 6 + 1);
+        var dieTwo = Math.floor(Math.random() * 6 + 1);
+        var diceSum = dieTwo + dieOne;
 
-		}	
+        if (diceSum !== 7) {
+            console.log("lost");
+            currentMoney--;
+            console.log(currentMoney)
+        }
+        else {
+            console.log("winner");
+            currentMoney = currentMoney + 4;
+            console.log(currentMoney);
 
-	}
-	console.log(maxAmountWon);
-	console.log(rollsAtMaxAmountWon);
-	console.log(rollCounter);
-	document.getElementById("outputInputBet").innerText = ("$" + inputBet.toFixed(2));
-	document.getElementById("outputRollCounter").innerText = rollCounter;
-	document.getElementById("outputMaxAmountWon").innerText = ("$" + maxAmountWon.toFixed(2));
-	document.getElementById("outputRollsAtMaxAmountWon").innerText = rollsAtMaxAmountWon;
-	document.getElementById("results").style.display = ""
-	document.getElementById("clickBtnAgain").innerText = "Play Again";
+            if (maxAmountWon < currentMoney){
+                maxAmountWon = currentMoney;
+                rollsAtMaxAmountWon = rollCounter;
+            }
+
+        }
+
+    }
+    console.log(maxAmountWon);
+    console.log(rollsAtMaxAmountWon);
+    console.log(rollCounter);
+    document.getElementById("outputInputBet").innerText = ("$" + inputBet.toFixed(2));
+    document.getElementById("outputRollCounter").innerText = rollCounter.toString();
+    document.getElementById("outputMaxAmountWon").innerText = ("$" + maxAmountWon.toFixed(2));
+    document.getElementById("outputRollsAtMaxAmountWon").innerText = rollsAtMaxAmountWon.toString();
+    document.getElementById("results").style.display = "";
+    document.getElementById("clickBtnAgain").innerText = "Play Again";
 }
 }
